e1e87c622f273a625b75e690b2eea38fcbec091d
rahul rai <rrai06125@gmail.com>
2021-09-30 14:35:40 +0530

    product weight value convertion was implemented

--- src/views/inventory/AddProduct.js
+++ src/views/inventory/AddProduct.js
@@ -899,7 +899,7 @@ function AddProduct(props) {
           vendors: response.data.vendors,
           country: response.data.country,
           weight_unit: response.data.weight_unit,
-          weight_value: response.data.weight_value,
+          weight_value:  (response.data.weight_unit === "kg") ? parseFloat(response.data.weight_value) / 1000 : (response.data.weight_unit === "mg") ?  parseFloat(response.data.weight_value) * 1000 : response.data.weight_unit ,
           sub_categories: response.data.sub_categories.map((sc) => sc.id),
         };
         setBasicData(initialBasicFieldsData);
@@ -1040,6 +1040,8 @@ function AddProduct(props) {
     label: label.toLowerCase().replace(/\W/g, ""),
     new: true,
   });
+
+  console.log("Initial Data------>>>>>>>",initialData)
 
   return (
     <>
