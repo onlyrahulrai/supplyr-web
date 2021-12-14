c69f0f0efa63ed856ac09e7245ea554a5978e133
Rahul Rai <rrai06125@gmail.com>
2021-12-14 14:04:49 +0530

    .

--- src/views/invoice/_Invoice.js
+++ src/views/invoice/_Invoice.js
@@ -1,67 +1,34 @@
-import React from "react"
-import {
-  Card,
-  CardBody,
-  Row,
-  Col,
-  Media,
-  Table,
-  InputGroup,
-  Input,
-  InputGroupAddon,
-  Button
-} from "reactstrap"
-import Breadcrumbs from "components/@vuexy/breadCrumbs/BreadCrumb"
-import logo from "../../assets/img/logo/logo.png"
-import { Mail, Phone, FileText, Download } from "react-feather"
+import BreadCrumbs from "components/@vuexy/breadCrumbs/BreadCrumb";
+import React,{useEffect} from "react";
 
-import "../../assets/scss/pages/invoice.scss"
+import { Row, Col, Table, CardBody, Card } from "reactstrap";
 
-class Invoice extends React.Component {
-  render() {
+// ** Styles
+import "../../assets/scss/pages/invoice.scss";
+
+const Invoice = () => {
+
+    useEffect(() => {
+        window.print()
+    },[])
+
     return (
-      <React.Fragment>
-        <Breadcrumbs
-          breadCrumbTitle="Invoice"
-          breadCrumbParent="Pages"
-          breadCrumbActive="Invoice"
+        <div>
+        <BreadCrumbs
+            breadCrumbTitle="Invoice"
+            breadCrumbParent="Orders"
+            breadCrumbActive="Invoice"
         />
-        <Row>
-          <Col className="mb-1 invoice-header" md="5" sm="12">
-            <InputGroup>
-              <Input placeholder="Email" />
-              <InputGroupAddon addonType="append">
-                <Button.Ripple color="primary" outline>
-                  Send Invoice
-                </Button.Ripple>
-              </InputGroupAddon>
-            </InputGroup>
-          </Col>
-          <Col
-            className="d-flex flex-column flex-md-row justify-content-end invoice-header mb-1"
-            md="7"
-            sm="12"
-          >
-            <Button
-              className="mr-1 mb-md-0 mb-1"
-              color="primary"
-              onClick={() => window.print()}
-            >
-              <FileText size="15" />
-              <span className="align-middle ml-50">Print</span>
-            </Button>
-            <Button.Ripple color="primary" outline>
-              <Download size="15" />
-              <span className="align-middle ml-50">Download</span>
-            </Button.Ripple>
-          </Col>
-          <Col className="invoice-wrapper" sm="12">
-            <Card className="invoice-page">
-              <CardBody>
-              <h3 className="text-center"><u>INVOICE</u></h3>
-                            <Row className="mt-3 ml-0 mr-0">
-                                <Col sm="4" className="border px-0">
-                                <div className="p-1">
+
+            
+                <Row>
+                    <Col className="invoice-wrapper">
+                    <Card className="invoice-page">
+                        <CardBody>
+                            <h3 className="text-center"><u>INVOICE</u></h3>
+                            <Row className="mt-3">
+                                <Col sm="4" className="p-0">
+                                <div className="border p-half">
                                     <small><strong>Exporter:</strong></small> <br />
                                 
                                     <div>
@@ -79,10 +46,10 @@ class Invoice extends React.Component {
                                     </div>
                                     </div>
                                 </Col>
-                                <Col sm="8" className="px-0">
-                                    <Row className="ml-0 mr-0">
-                                        <Col sm="6" className="px-0">
-                                        <div className="p-1 border">
+                                <Col sm="8" >
+                                    <Row>
+                                        <Col sm="6" className="p-0">
+                                        <div className="border p-half">
                                                 <small><strong>Invoice No. & Date:</strong></small><br />
                                                 <div>
                                                     <strong>PGI/EXP/----/21-22</strong><br />
@@ -91,30 +58,30 @@ class Invoice extends React.Component {
                                             </div>
                                             
                                         </Col>
-                                        <Col sm="6" className="pl-0 border">
-                                            <div className="p-1">
+                                        <Col sm="6" className="p-0">
+                                            <div className="border p-half">
                                                 <small><strong>Exporter's Ref,:</strong></small><br /><br /><br />
                                             </div>
                                         </Col>
                                     </Row>
-                                    <Row className="ml-0 mr-0"> 
-                                        <Col sm="12" className="px-0 p-1  border">
+                                    <Row>
+                                        <Col sm="12" className="p-0 border">
                                             <small><strong>Buyer's Order No.& Date</strong></small>
                                             <div>
                                                 <span><strong>NIL.,  DATED:--/--/----</strong></span>
                                             </div>
                                         </Col>
                                     </Row>
-                                    <Row className="ml-0 mr-0">
+                                    <Row>
                                         <Col sm="12" className="p-6 border">
                                             <small><strong>Other Reference (s)</strong></small>
                                         </Col>
                                     </Row>
                                 </Col>
                             </Row>
-                            <Row className="ml-0 mr-0">
-                            <Col sm="4" className="px-0">
-                                <div className="border p-1 border-top-0" style={{minHeight:"180px"}}>
+                            <Row>
+                            <Col sm="4" className="p-0">
+                                <div className="border border-top-0 p-half" style={{minHeight:"152px"}}>
                                     <small><strong>Consignee:</strong></small> <br />
 
                                     <div>
@@ -122,17 +89,17 @@ class Invoice extends React.Component {
                                     </div>
                                 </div>
                             </Col>
-                            <Col sm="8" className="pl-0 border">
-                                <div className="p-1 border-top-0">
+                            <Col sm="8" className="p-0">
+                                <div className="border border-top-0 p-half">
                                     <small>IEC. NO. ----,  <strong>GSTIN: ----</strong></small> <br />
                                     <div>
                                         <span><strong>Buyer (If other than consignee)</strong></span><br />
                                     </div>
                                 </div>
 
-                                <Row className="border-top ml-0 mr-0">
-                                    <Col sm="6" className="px-0  border-right" >
-                                        <div className="p-1 border-top-0">
+                                <Row>
+                                    <Col sm="6" className="pr-0" >
+                                        <div className="border border-top-0 p-half">
                                             <small><strong>Country of Origin </strong></small> <br />
 
                                             <div>
@@ -141,8 +108,8 @@ class Invoice extends React.Component {
                                             </div>
                                         </div>    
                                     </Col>
-                                    <Col sm="6" className="px-0" >
-                                        <div className="p-1 border-top-0">
+                                    <Col sm="6" className="pl-0" >
+                                        <div className="border border-top-0 p-half">
                                             <small><strong>Country of final </strong></small> <br />
                                             <div>
                                                 <small><strong>Destination</strong></small> <br /><br /><br />
@@ -152,50 +119,50 @@ class Invoice extends React.Component {
                                 </Row>
                             </Col>
                             </Row>
-                            <Row className="ml-0 mr-0">
-                                <Col sm="7" className="px-0">
-                                    <Row className="ml-0 mr-0">
-                                        <Col sm="4" className="px-0">
-                                            <div className="border p-1 border-top-0">
+                            <Row>
+                                <Col sm="7">
+                                    <Row>
+                                        <Col sm="4" className="p-0">
+                                            <div className="border border-top-0 p-half">
                                                 <small><strong>Pre-Carriage by:</strong></small> <br /><br />
                                                 <div>
                                                     <strong>ROAD/RAIL</strong><br />
                                                 </div>
                                             </div>
                                         </Col>
-                                        <Col sm="8" className="px-0">
-                                            <div className="border p-1 border-top-0">
+                                        <Col sm="8" className="p-0">
+                                            <div className="border border-top-0 p-half">
                                                 <small><strong>Place of Receiptby Pre-carrier:</strong></small> <br /><br /><br />
                                             </div>
                                         </Col>
                                     </Row>
-                                    <Row className="ml-0 mr-0"> 
-                                        <Col sm="4" className="px-0">
-                                            <div className="border p-1 border-top-0">
-                                                <small><strong>Vessel No.</strong></small> <br /><br /><br />
+                                    <Row>
+                                        <Col sm="4" className="p-0">
+                                            <div className="border border-top-0 p-half">
+                                                <small><strong>Vessel No.</strong></small> <br /><br />
                                             </div>
                                         </Col>
-                                        <Col sm="8" className="px-0">
-                                            <div className="border p-1 border-top-0">
-                                                <small><strong>Port of Loading</strong></small> <br /><br /><br />
+                                        <Col sm="8" className="p-0">
+                                            <div className="border border-top-0 p-half">
+                                                <small><strong>Port of Loading</strong></small> <br /><br />
                                             </div>
                                         </Col>
                                     </Row>
-                                    <Row className="ml-0 mr-0">
-                                        <Col sm="4" className="px-0">
-                                            <div className="border p-1 border-top-0 border-bottom-0">
-                                                <small><strong>Port of Disch.</strong></small> <br /><br /><br />
+                                    <Row>
+                                        <Col sm="4" className="p-0">
+                                            <div className="border border-top-0 p-half">
+                                                <small><strong>Port of Disch.</strong></small> <br /><br />
                                             </div>
                                         </Col>
-                                        <Col sm="8" className="px-0">
-                                            <div className="border p-1 border-top-0 border-bottom-0">
-                                                <small><strong>Final Destinat.</strong></small> <br /><br /><br />
+                                        <Col sm="8" className="p-0">
+                                            <div className="border border-top-0 p-half">
+                                                <small><strong>Final Destinat.</strong></small> <br /><br />
                                             </div>
                                         </Col>
                                     </Row>
                                 </Col>
-                                <Col sm="5" className="border px-0">
-                                    <div className=" p-1 border-top-0" style={{minHeight:"180px"}}>
+                                <Col sm="5" className="p-0">
+                                    <div className="border border-top-0 p-half" style={{minHeight:"187px"}}>
                                         <small><strong>Terms of Delivery & Payment:</strong></small> <br /><br />
                                         <div>
                                             <strong className="pl-1">  C I F</strong> <br /><br />
@@ -205,7 +172,7 @@ class Invoice extends React.Component {
                                     </div>
                                 </Col>
                             </Row>
-                            <Row className="border  pt-0 pb-0 ml-0 mr-0" style={{minHeight:"300px"}}>
+                            <Row className="border  pt-0 pb-0" style={{minHeight:"300px"}}>
                                 <Col sm="2">
                                     <small>
                                         <strong>Marks & & </strong>
@@ -249,7 +216,7 @@ class Invoice extends React.Component {
                                     </div>
                                 </Col>
                             </Row>
-                            <Row className="border border-bottom-0 ml-0 mr-0" style={{minHeight:"200px"}}>
+                            <Row className="border border-bottom-0" style={{minHeight:"200px"}}>
                                 <Col sm="8">
                                     <small><strong>Amount Chargeable (In Words)</strong></small>
                                 </Col>
@@ -257,7 +224,7 @@ class Invoice extends React.Component {
                                     <small><strong>Total:</strong></small>
                                 </Col>
                             </Row>
-                            <Row className="border-top-0 border ml-0 mr-0">
+                            <Row className="border-top-0 border">
                                 <Col sm="8">
                                     <small><strong><u>DECLARATION</u></strong></small><br />
                                     <small><strong>We declare that this invoice shows the actual price of the goods described</strong></small><br />
@@ -267,13 +234,12 @@ class Invoice extends React.Component {
                                     <small><strong>Signature & Date:</strong></small>
                                 </Col>
                             </Row>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </React.Fragment>
-    )
-  }
-}
+                        </CardBody>
+                    </Card>
+                    </Col>
+                </Row>
+            </div>
+    );
+};
 
-export default Invoice
+export default Invoice;
