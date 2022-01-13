06ee164e3a97adb381fd49b429211ecb2a59e33d
Talha <abutalhadanish@gmail.com>
2022-01-13 18:46:05 +0530

    Currency representation workaround

--- src/components/utils/PriceDisplay.js
+++ src/components/utils/PriceDisplay.js
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import { numberFormatter } from "utility/general";
 
 const PriceDisplay = ({ profile, amount, styles = null,classes=null }) => {
-  let currency_representation = profile.currency_representation;
+  let currency_representation = profile.currency_representation ?? "$ {{amount}}";
 
   const priceFormatter = (amount) => {
     return currency_representation.search("{{amount}}") !== -1 ? currency_representation.replace("{{amount}}",numberFormatter(amount)) : currency_representation.search("{{amount_no_decimals}}") !== -1 ? currency_representation.replace("{{amount_no_decimals}}",numberFormatter(parseInt(amount))) : `${currency_representation}${amount}`
